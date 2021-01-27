22db4f4cd9e72cfab8c2159db340f8c44af7f5e9
David Millan <hello@davidmillan.me>
2021-01-27 22:16:12 +0000

    Creating Arrays

--- Creating/app.js
+++ Creating/app.js
@@ -1,1 +1,15 @@
+/* literal */
+let arr1 = [];
+let arr5 = [,,,];
+let arr2 = [1, 2, 3, 4, true, "array", { one: 1 }, [3, 4]];
 
+/* constructor */
+let arr3 = new Array();
+let arr4 = new Array(10);
+let arr6 = new Array(1,2,3,4,5,true);
+
+/* method */
+let arr7 = Array.of(5);
+
+
+console.log(arr7);
