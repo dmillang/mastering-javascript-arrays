ea9a19c9b1d358f636c71bc8cb70a99985b10577
David Millan <hello@davidmillan.me>
2021-01-27 22:42:57 +0000

    Using Array.fill to Create an Array

--- Creating/app.js
+++ Creating/app.js
@@ -1,16 +1,20 @@
-/* literal */
-let arr1 = [];
-let arr5 = [,,,];
-let arr2 = [1, 2, 3, 4, true, "array", { one: 1 }, [3, 4]];
+// /* literal */
+// let arr1 = [];
+// let arr5 = [,,,]; /* sparse array */
+// let arr2 = [1, 2, 3, 4, true, "array", { one: 1 }, [3, 4]];
 
-/* constructor */
-let arr3 = new Array();
-let arr4 = new Array(10);
-let arr6 = new Array(1,2,3,4,5,true);
+// /* constructor */
+// let arr3 = new Array();
+// let arr4 = new Array(10);
+// let arr6 = new Array(1,2,3,4,5,true);
 
-/* method */
-let arr7 = Array.of(5);
-let arr8 = Array.of(5,3,8,true);
+// /* method */
+// let arr7 = Array.of(5);
+// let arr8 = Array.of(5,3,8,true);
 
+/* Array.fill() method */
+let scores = Array(10).fill(5);
+let scores2 = [1, 2, 3, 4, 5, , , , ,].fill("fillIt!", 5, 100);
 
-console.log(arr7);
+console.log(scores);
+console.log(scores2);
