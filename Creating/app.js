f6c19f5bea20a70c4341fa7e74fbec8bfa647411
David Millan <hello@davidmillan.me>
2021-01-28 22:57:00 +0000

    deleting array elements

--- Creating/app.js
+++ Creating/app.js
@@ -19,19 +19,24 @@
 // console.log(scores);
 // console.log(scores2);
 
-/* Reading and writing */
-let arr9 = [1, 2, 3, 4, true, "array", { one: 1 }, [3, 4]];
-let arrElement = arr9[6];
-arr9[6] = "Max";
+// /* Reading and writing */
+// let arr9 = [1, 2, 3, 4, true, "array", { one: 1 }, [3, 4]];
+// let arrElement = arr9[6];
+// arr9[6] = "Max";
 
-/* Sparse Arrays */
-let arr10 = [1, 2, 3, 4, 5];
-arr10[20] = 19;
-arr10[14] = "fourteen";
+// /* Sparse Arrays */
+// let arr10 = [1, 2, 3, 4, 5];
+// arr10[20] = 19;
+// arr10[14] = "fourteen";
 
-/* Length */
-let arr11 = [1,2,3,4,5];
-arr11[arr11.length] = 6;
-arr11.length = 20;
+// /* Length */
+// let arr11 = [1,2,3,4,5];
+// arr11[arr11.length] = 6;
+// arr11.length = 20;
 
-console.log(arr11);
+let arr12 = [1,2,3,4,5,6,7];
+
+delete arr12[5]; // doesn't adjust elements, array keeps being the same length
+
+console.log(arr12);
+
