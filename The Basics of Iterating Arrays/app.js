18defff1c32eaf6e777851a366d701eac3a724e6
David Millan <hello@davidmillan.me>
2021-01-30 14:14:43 +0000

    Using the for of Loop

--- The Basics of Iterating Arrays/app.js
+++ The Basics of Iterating Arrays/app.js
@@ -1,14 +1,19 @@
 // let arr = [3, 15, 25, 6, 8, 12];
-
 // for (let i = 0; i < arr.length; i++) {
 //   arr[i] *= 5;
 // }
-
 // console.log(arr);
 
-const arr = [3,7,10,50,7];
+// const arr = [3,7,10,50,7];
+// for (const key in arr) {
+//   console.log(key);
+//   console.log(arr[key]);
+// }
 
-for (const key in arr) {
-  console.log(key);
-  console.log(arr[key]);
+const arr = [1,2,3,4,5];
+let total = 0;
+
+for (const val of arr) {
+  total += val;
 }
+console.log(total);
