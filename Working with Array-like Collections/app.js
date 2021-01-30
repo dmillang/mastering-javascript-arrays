3f96fdec987cd02647552a8403097049dc5ab924
David Millan <hello@davidmillan.me>
2021-01-30 18:24:03 +0000

    create capitalizeWords function

--- Working with Array-like Collections/app.js
+++ Working with Array-like Collections/app.js
@@ -1,19 +1,19 @@
-let arr = [1, 2, 3];
+// let arr = [1, 2, 3];
 
-let obj = {
-    1: 'one',
-    2: 'two',
-    3: 'three'
-};
+// let obj = {
+//     1: 'one',
+//     2: 'two',
+//     3: 'three'
+// };
 
 // for (let val of obj) {
 //     console.log(val);
 // };
 
-let map = new Map();
-map.set(1,'one');
-map.set(2,'two');
-map.set(3,'three');
+// let map = new Map();
+// map.set(1,'one');
+// map.set(2,'two');
+// map.set(3,'three');
 
 // for (let val of map) {
 //     console.log(val);
@@ -21,26 +21,63 @@ map.set(3,'three');
 
 // console.log(Array.isArray(arr)); // checks if it's an array by using a static method
 
-let divs = document.querySelectorAll('div');
+// let divs = document.querySelectorAll('div');
 
 // for (let val of divs) {
 //     console.log(val);
 // };
 
-var mapArray = [...map]; // spread operator
-var mapArray2 = Array.from(map); // static method
-console.log(mapArray);
-console.log(mapArray2);
+// var mapArray = [...map]; // spread operator
+// var mapArray2 = Array.from(map); // static method
+// console.log(mapArray);
+// console.log(mapArray2);
 
-var mapDivs = [...divs];
-console.log(mapDivs);
+// var mapDivs = [...divs];
+// console.log(mapDivs);
 
-let obj2 = {
-    0: 'zero',
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    length: 4
+// let obj2 = {
+//     0: 'zero',
+//     1: 'one',
+//     2: 'two',
+//     3: 'three',
+//     length: 4
+// };
+
+// console.log(Array.from(obj2));
+
+let string = "hello there";
+let stringArr = Array.from(string);
+
+for (var i = 0; i < stringArr.length; i++) {
+  if (stringArr.indexOf(stringArr[i]) === 0) {
+    stringArr[i] = stringArr[i].toUpperCase();
+  } else if (stringArr[i] === " ") {
+    stringArr[i + 1] = stringArr[i + 1].toUpperCase();
+  }
+}
+
+const capitalizeWords = function (str) {
+    let string = str;
+    let stringArr = Array.from(string);
+
+  for (var i = 0; i < stringArr.length; i++) {
+    if (stringArr.indexOf(stringArr[i]) === 0) {
+      stringArr[i] = stringArr[i].toUpperCase();
+    } else if (stringArr[i] === " ") {
+      stringArr[i + 1] = stringArr[i + 1].toUpperCase();
+    }
+
+    string = string.slice(1);
+    string += stringArr[i];
+  }
+
+  return string;
+
 };
 
-console.log(Array.from(obj2));
+let test = capitalizeWords("new events coming");
+
+console.log(test);
+
+console.log(stringArr);
+console.log(string);
