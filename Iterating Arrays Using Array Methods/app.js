37342f7570c6d514152fbc4acef0163d7c8e7322
David Millan <hello@davidmillan.me>
2021-02-03 14:25:19 +0000

    Using the map Method

--- Iterating Arrays Using Array Methods/app.js
+++ Iterating Arrays Using Array Methods/app.js
@@ -1,6 +1,5 @@
-
-let arr = [3, 15, 25, 6, 8, 12],
-    total = 0;
+// let arr = [3, 15, 25, 6, 8, 12],
+//     total = 0;
 
 // for (let val of arr) {
 //     total += val;
@@ -12,40 +11,56 @@ let arr = [3, 15, 25, 6, 8, 12],
 
 // console.log(total);
 
-arr.forEach(function(val, idx, arr) {
-    arr[idx] = val * val;
+// arr.forEach(function(val, idx, arr) {
+//     arr[idx] = val * val;
+// });
+
+// console.log(arr);
+
+// let objArr = [{
+//     firstName: "Steven",
+//     lastName: "Hancock",
+//     score: 90,
+//     pass: null
+// },{
+//     firstName: "Lynette",
+//     lastName: "Jorgensen",
+//     score: 100,
+//     pass: null
+// },{
+//     firstName: "Andrew",
+//     lastName: "Wilson",
+//     score: 69,
+//     pass: null
+// },{
+//     firstName: "Annika",
+//     lastName: "Turner",
+//     score: 82,
+//     pass: null
+// }];
+
+// objArr.forEach((val) => {
+//     if (val.score >= 70) {
+//         val.pass = true;
+//     } else {
+//         val.pass = false;
+//     }
+// });
+
+// console.log(objArr);
+
+let arr = [3, 15, 25, 6, 8, 12];
+
+// let newArr = arr.map((val, idx, theArray) => {
+//     console.log(val, idx, theArray);
+//     return val * val
+// });
+// console.log(newArr);
+
+let objArr = arr.map((val, idx, theArray) => {
+    console.log(val, idx, theArray);
+    return {val};
 });
 
 console.log(arr);
-
-let objArr = [{
-    firstName: "Steven",
-    lastName: "Hancock",
-    score: 90,
-    pass: null
-},{
-    firstName: "Lynette",
-    lastName: "Jorgensen",
-    score: 100,
-    pass: null
-},{
-    firstName: "Andrew",
-    lastName: "Wilson",
-    score: 69,
-    pass: null
-},{
-    firstName: "Annika",
-    lastName: "Turner",
-    score: 82,
-    pass: null
-}];
-
-objArr.forEach((val) => {
-    if (val.score >= 70) {
-        val.pass = true;
-    } else {
-        val.pass = false;
-    }
-});
-
 console.log(objArr);
