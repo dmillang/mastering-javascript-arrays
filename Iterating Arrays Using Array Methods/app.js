8a8c587ac330923c68ef3ffbbb2b8e9696282afb
David Millan <hello@davidmillan.me>
2021-02-03 14:14:56 +0000

    Using the forEach Method

---
+++ Iterating Arrays Using Array Methods/app.js
@@ -0,0 +1,51 @@
+
+let arr = [3, 15, 25, 6, 8, 12],
+    total = 0;
+
+// for (let val of arr) {
+//     total += val;
+// };
+
+// arr.forEach((val) => {
+//     total += val
+// });
+
+// console.log(total);
+
+arr.forEach(function(val, idx, arr) {
+    arr[idx] = val * val;
+});
+
+console.log(arr);
+
+let objArr = [{
+    firstName: "Steven",
+    lastName: "Hancock",
+    score: 90,
+    pass: null
+},{
+    firstName: "Lynette",
+    lastName: "Jorgensen",
+    score: 100,
+    pass: null
+},{
+    firstName: "Andrew",
+    lastName: "Wilson",
+    score: 69,
+    pass: null
+},{
+    firstName: "Annika",
+    lastName: "Turner",
+    score: 82,
+    pass: null
+}];
+
+objArr.forEach((val) => {
+    if (val.score >= 70) {
+        val.pass = true;
+    } else {
+        val.pass = false;
+    }
+});
+
+console.log(objArr);
