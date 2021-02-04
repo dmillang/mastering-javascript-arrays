dc1604452eab8418d9f21145018951a76b863b27
David Millan <hello@davidmillan.me>
2021-02-04 17:40:58 +0000

    Combining Arrays Together

--- Combining Arrays and Extracting Values/app.js
+++ Combining Arrays and Extracting Values/app.js
@@ -1,20 +1,21 @@
-let stmt =
-  "The best part of a sunset is the muted color that appears in anticipation of the deep reds, oranges and golds that will hopefully come later.";
+// let stmt =
+//   "The best part of a sunset is the muted color that appears in anticipation of the deep reds, oranges and golds that will hopefully come later.";
 
-let words = stmt.split(" ");
+// let words = stmt.split(" ");
 
-console.log(words.join(" "));
+// console.log(words.join(" "));
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-let str = arr.join();
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let str = arr.join();
 
-console.log(str);
+// console.log(str);
 
-// let array1 = [5, 10, 15, 20, 25],
-//     array2 = [2, 4, 6, 8, 10, 12],
-//     array3 = ["abc", "def", "ghi", "jkl"];
+let array1 = [5, 10, 15, 20, 25],
+  array2 = [2, 4, 6, 8, 10, 12],
+  array3 = ["abc", "def", "ghi", "jkl"];
 
-// let newArray = [...array1,...array2,...array3];
+// let newArray = [...array1,...array2,...array3, 1, 3, 4, 5];
 
-// console.log(newArray);
+let newArray = array3.concat(array1, array2, [0, -1, -2, -3]);
 
+console.log(newArray);
