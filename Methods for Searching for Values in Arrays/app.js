cffb87c13d2433cea22a7583a713eed5cc51d162
David Millan <hello@davidmillan.me>
2021-02-04 22:30:43 +0000

    Checking an Array Using includes

--- Methods for Searching for Values in Arrays/app.js
+++ Methods for Searching for Values in Arrays/app.js
@@ -1,10 +1,18 @@
-let students = ["Steven", "Lynette", "Kaylie", "LJ", "Joshua", "Sarah", "Emily"],
-    scores = [80, 90, 55, 60, 85, 95, 25, 100];
+let students = [
+    "Steven",
+    "Lynette",
+    "Kaylie",
+    "LJ",
+    "Joshua",
+    "Sarah",
+    "Emily",
+  ],
+  scores = [80, 90, 55, 60, 85, 95, 25, 100];
 
 let numLoc = scores.indexOf(25);
 let numLoc2 = scores.indexOf(400);
 
-let strLoc = students.indexOf('LJ');
+let strLoc = students.indexOf("LJ");
 
 console.log(numLoc);
 console.log(numLoc2);
@@ -15,3 +23,10 @@ if (scores.indexOf(100) > -1) {
 } else {
   console.log("Array does NOT contain 100");
 }
+
+if (scores.includes(7) ? console.log(true) : console.log(false)); // false
+
+let arrCon = (arr) => (arr.includes(80) ? true : false);
+
+console.log(arrCon(scores)); // true
+console.log(arrCon(students)); // false
