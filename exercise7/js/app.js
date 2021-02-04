a53e1fb219f130bb4903c9975e73b40d4967c056
David Millan <hello@davidmillan.me>
2021-02-04 15:02:58 +0000

    Exercise 7

--- exercise7/js/app.js
+++ exercise7/js/app.js
@@ -1,3 +1,20 @@
 //Convert this string to an array. Then create a new array that doesn't include any of the articles (a, an, the).
 
 let stmt = "The best part of a sunset is the muted color that appears in anticipation of the deep reds, oranges and golds that will hopefully come later.";
+
+// let stmtArr = stmt.toLowerCase().replace("."," ").split(" ").filter((val) => val !== "the").filter((val) => val !== "a").filter((val) => val !== "an");
+
+// let stmtArr = stmt.toLowerCase().split(" ").filter((val) => {
+//   if (val !== "the" && val !== "a" && val !== "an") {
+//     return val;
+//   }
+// });
+// console.log(stmtArr);
+
+// let noArticles = stmt.split(" ").filter(function(val) {
+//   return !/\ba\b|\ban\b|\bthe\b/i.test(val);
+// });
+
+let noArticles = stmt.split(" ").filter((val) => !/\ba\b|\ban\b|\bthe\b/i.test(val));
+
+console.log(noArticles);
