6c980eae78fcc94c196a094169d6b199f47b4e1e
David Millan <hello@davidmillan.me>
2021-02-04 17:37:10 +0000

    Using the join Method

---
+++ Combining Arrays and Extracting Values/app.js
@@ -0,0 +1,20 @@
+let stmt =
+  "The best part of a sunset is the muted color that appears in anticipation of the deep reds, oranges and golds that will hopefully come later.";
+
+let words = stmt.split(" ");
+
+console.log(words.join(" "));
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let str = arr.join();
+
+console.log(str);
+
+// let array1 = [5, 10, 15, 20, 25],
+//     array2 = [2, 4, 6, 8, 10, 12],
+//     array3 = ["abc", "def", "ghi", "jkl"];
+
+// let newArray = [...array1,...array2,...array3];
+
+// console.log(newArray);
+
